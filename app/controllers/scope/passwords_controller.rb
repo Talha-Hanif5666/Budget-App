9a8f26e2903377055ec997d7d99cfe8d34d5b99c
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-01 18:23:22 +0500

    Remove linter errors

--- app/controllers/scope/passwords_controller.rb
+++ app/controllers/scope/passwords_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class [scope]::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
--- app/controllers/scope/unlocks_controller.rb
+++ app/controllers/scope/unlocks_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class [scope]::UnlocksController < Devise::UnlocksController
   # GET /resource/unlock/new
   # def new
