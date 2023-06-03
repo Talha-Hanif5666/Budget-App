6d054c6d4c653b3d0e3f9a34415ab5c4130bfcb4
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-03 14:05:46 +0500

    Fixed errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validation tests' do
-    first_user = User.create(name: 'Spice', email: 'testingsone@gmail.com', password: 'Leonileo')
-
-    it 'is is created succefully' do
-      expect(first_user).to be_valid
+  describe 'User Model' do
+    before do
+      @user = User.new(name: 'Username', email: 'user@gmail.com', password: 'password')
     end
 
-    it 'is invalid without a name' do
-      first_user.name = nil
-      expect(first_user).to_not be_valid
+    it 'is not valid without a name' do
+      @user.name = nil
+      expect(@user).to_not be_valid
     end
 
-    it 'is invalid without an email' do
-      first_user.email = nil
-      expect(first_user).to_not be_valid
+    it 'is not valid without an email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
     end
 
-    it 'is invalid without a password' do
-      first_user.password = nil
-      expect(first_user).to_not be_valid
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
     end
+
+    it 'is valid with valid attributes' do
+      expect(@user).to be_valid
+    end
+
   end
 end
